48e9fc38eb80e02350823726f7571ba34977abfd
Mudabir <mudabirahmad1994@gmail.com>
2018-05-20 14:12:14 -0700

    revesred something things

--- biasweb/settings.py
+++ biasweb/settings.py
@@ -76,19 +76,8 @@ WSGI_APPLICATION = 'biasweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-if 'RDS_HOSTNAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES = {
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'django_biasweb',
